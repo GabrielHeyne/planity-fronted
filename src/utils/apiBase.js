f90ff635563a54eefe12d926b43d57699dbb9d30
Gabriel <gabriel.heyne@gmail.com>
2025-05-16 12:22:18 -0400

    🔥 Cleanup: remove cache files and apply updated .gitignore

--- src/utils/apiBase.js
+++ src/utils/apiBase.js
@@ -1,5 +1,5 @@
 // src/utils/apiBase.js
-const ENTORNO = "prod"; // ⬅️ cambia a "prod" si necesitas
+const ENTORNO = "dev"; // ⬅️ cambia a "prod" si necesitas
 
 const URLS = {
   dev: "http://127.0.0.1:8001",
